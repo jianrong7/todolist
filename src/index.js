a18d0fef86faf4f0293185a6fb3356dab8e2594d
Loh Jian Rong <ipwnjianrong@gmail.com>
2020-05-31 19:59:20 +0800

    Added minor changes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import TodoContainer from './todoContainer';
+import Todo from './todo';
+import userInterface from './userInterface';
 
 const initializeOrganizer = (() => {
     const defaultContainer = TodoContainer("Get started");

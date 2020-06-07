c7c7354849e72084cb69bbb7b571fe6554389d01
Loh Jian Rong <ipwnjianrong@gmail.com>
2020-06-07 20:14:28 +0800

    Reset

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import TodoContainer from './todoContainer';
-import Todo from './todo';
-import userInterface from './userInterface';
-
-const initializeOrganizer = (() => {
-    const defaultContainer = TodoContainer("Get started");
-
-    
-})();
